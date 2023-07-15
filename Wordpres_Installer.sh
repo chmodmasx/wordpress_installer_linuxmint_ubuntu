7432305ddf5991f71d7c5038d97dbf058bad1e12
EspadaRunica <44514442+EspadaRunica@users.noreply.github.com>
2023-07-15 11:37:54 -0300

    Wordpres_Installer.sh

--- Wordpres_Installer.sh
+++ Wordpres_Installer.sh
@@ -1,11 +1,12 @@
 #/bin/sh
-
+echo "\n"
 echo "   ____  _____                     _       ____              _           "
 echo "  / __ \| ____|___ _ __   __ _  __| | __ _|  _ \ _   _ _ __ (_) ___ __ _ "
 echo " / / _\` |  _| / __| '_ \ / _\` |/ _\` |/ _\` | |_) | | | | '_ \| |/ __/ _\` |"
 echo "| | (_| | |___\__ \ |_) | (_| | (_| | (_| |  _ <| |_| | | | | | (_| (_| |"
 echo " \ \__,_|_____|___/ .__/ \__,_|\__,_|\__,_|_| \_\__,__|_| |_|_|\___\__,_|"
 echo "  \____/          |_|                                                    "
+echo "\n"
 
 
 install_dir="/var/www/html"
@@ -13,18 +14,17 @@ install_dir="/var/www/html"
 db_name="wp`date +%s`"
 db_user=$db_name
 db_password=`date |md5sum |cut -c '1-12'`
-sleep 1
-mysqlrootpass=`date |md5sum |cut -c '1-12'`
-sleep 1
-
-#### Instalar paquetes para https y mysql
-apt -y update 
-apt -y upgrade
-apt -y install apache2
-apt -y install mysql-server
+mysqlrootpass=$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9' | head -c 12)
 
 
-#### Borra el directorio html por defecto y habilitamos apache
+#### Instalación de paquetes
+apt update && apt upgrade -y 
+apt install apache2 php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml mysql-server -y
+
+a2enmod rewrite
+
+
+#### Borra el archivo html por defecto y habilitamos apache
 rm /var/www/html/index.html
 systemctl enable apache2
 systemctl start apache2
@@ -35,15 +35,12 @@ systemctl enable mysql
 systemctl start mysql
 
 /usr/bin/mysql -e "USE mysql;"
-/usr/bin/mysql -e "UPDATE user SET Password=PASSWORD($mysqlrootpass) WHERE user='root';"
-/usr/bin/mysql -e "FLUSH PRIVILEGES;"
+/usr/bin/mysql -e "USE mysql; UPDATE user SET authentication_string=CONCAT('*', UPPER(SHA2('$mysqlrootpass', 256))) WHERE user='root'; FLUSH PRIVILEGES;"
 touch /root/.my.cnf
 chmod 640 /root/.my.cnf
 echo "[client]">>/root/.my.cnf
 echo "user=root">>/root/.my.cnf
 echo "password="$mysqlrootpass>>/root/.my.cnf
-####Install PHP
-apt -y install php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml
 
 sed -i '0,/AllowOverride\ None/! {0,/AllowOverride\ None/ s/AllowOverride\ None/AllowOverride\ All/}' /etc/apache2/apache2.conf #Allow htaccess usage
 
@@ -95,12 +92,6 @@ grep -A50 'table_prefix' $install_dir/wp-config.php > /tmp/wp-tmp-config
 /usr/bin/mysql -u root -e "CREATE DATABASE $db_name"
 /usr/bin/mysql -u root -e "CREATE USER '$db_name'@'localhost' IDENTIFIED WITH mysql_native_password BY '$db_password';"
 /usr/bin/mysql -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';"
- 
-######Display generated passwords to log file.
-echo "Database Name: " $db_name
-echo "Database User: " $db_user
-echo "Database Password: " $db_password
-echo "Mysql root password: " $mysqlrootpass
 
 
 ### Algunos limites de PHP que recomiendo
@@ -109,12 +100,14 @@ sudo sed -i 's/upload_max_filesize = .*/upload_max_filesize = 4192M/' /etc/php/8
 sudo sed -i 's/post_max_size = .*/post_max_size = 4192M/' /etc/php/8.1/apache2/php.ini
 sudo systemctl restart apache2
 
-echo "   ____  _____                     _       ____              _           "
-echo "  / __ \| ____|___ _ __   __ _  __| | __ _|  _ \ _   _ _ __ (_) ___ __ _ "
-echo " / / _\` |  _| / __| '_ \ / _\` |/ _\` |/ _\` | |_) | | | | '_ \| |/ __/ _\` |"
-echo "| | (_| | |___\__ \ |_) | (_| | (_| | (_| |  _ <| |_| | | | | | (_| (_| |"
-echo " \ \__,_|_____|___/ .__/ \__,_|\__,_|\__,_|_| \_\__,__|_| |_|_|\___\__,_|"
-echo "  \____/          |_|                                                    "
-
+######Display generated passwords to log file.
+echo "\n"
+echo "AQUÍ LOS DATOS DE TU INSTALACIÓN DE WORDPRESS Y SU BASE DE DATOS"
+echo "Database Name: " $db_name
+echo "Database User: " $db_user
+echo "Database Password: " $db_password
+echo "Mysql root password: " $mysqlrootpass
+echo "\n"
 
 echo "Ingrese a: http://localhost o bien introduzca su dirección web"
+echo "Gracias por utilizar el script de @EspadaRunica"
