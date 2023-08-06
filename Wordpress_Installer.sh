b4caf5d597b9ccd6bcb62eadb1eeb48043c05ffd
chmodmasx <daninicob@gmail.com>
2023-08-06 04:21:43 -0300

    Se agrega a la instalación los paquetes redis-server y php-redis.

--- Wordpress_Installer.sh
+++ Wordpress_Installer.sh
@@ -25,7 +25,7 @@ echo "phpmyadmin phpmyadmin/app-password-confirm password $db_password" | debcon
 
 #### Instalar paquetes para https y mysql
 apt-get update && apt-get upgrade -y
-apt-get install -y apache2 memcached php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml php-zip php-imagick php-memcache mysql-server phpmyadmin
+apt-get install -y apache2 memcached php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml php-zip php-imagick php-memcache redis-server php-redis mysql-server phpmyadmin
 
 a2enmod rewrite
 
