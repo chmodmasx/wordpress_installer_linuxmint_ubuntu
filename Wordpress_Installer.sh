56508f8f7dea602bc2c4a4a0bb3b1612a7560014
chmodmasx <daninicob@gmail.com>
2023-09-09 23:38:43 -0300

    Update Wordpress_Installer.sh

--- Wordpress_Installer.sh
+++ Wordpress_Installer.sh
@@ -9,9 +9,8 @@ echo "  \____/          |_|                                                    "
 
 echo "\n"
 
-#!/bin/bash
-
-echo "Cuando te pregunte por una contraseña durante la ejecución solo presiona Enter"
+echo "Le pedira una contraseña a lo largo de la instalación, puede presionar Enter simplemente"
+echo "\n"
 read -p "Ingrese su nombre de dominio: " DOMAIN_USER
 
 DB_NAME="wp$(date +%s)"
@@ -129,6 +128,12 @@ sudo sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
 sudo sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
 sudo sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/wp-config.php
 
+# Obtener las claves secretas de WordPress y agregarlas al archivo wp-config.php
+#grep -A50 'table_prefix' /var/www/html/wp-config.php > /tmp/wp-tmp-config
+#sed -i '/^\/\*\*#@/,/^ \*\/$/d' /var/www/html/wp-config.php
+#secret_keys=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+#echo "$secret_keys" >> /var/www/html/wp-config.php
+
 # Mostrar las contraseñas generadas en el archivo de registro
 echo "Aquí tus datos:"
 echo "Database Name: $db_name"
@@ -137,12 +142,5 @@ echo "Database Password: $db_password"
 echo "MySQL Root Password: $mysqlrootpass"
 
 echo "Instalación de WordPress completada. Accede a tu sitio en http://$DOMAIN"
-
-
-# Obtener las claves secretas de WordPress y agregarlas al archivo wp-config.php
-grep -A50 'table_prefix' /var/www/html/wp-config.php > /tmp/wp-tmp-config
-sed -i '/**#@/,/$p/d' /var/www/html/wp-config.php
-secret_keys=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
-echo "$secret_keys" >> /var/www/html/wp-config.php
 
 echo "Gracias por utilizar el script de @EspadaRunica"
