251e990530e1510c1ee7ca7bcaf59fcf42c5904a
chmodmasx <daninicob@gmail.com>
2023-09-12 11:27:59 -0300

    Update Wordpress_Installer.sh

--- Wordpress_Installer.sh
+++ Wordpress_Installer.sh
@@ -14,23 +14,23 @@ echo "\n"
 read -p "Ingrese su nombre de dominio (por ejemplo mintlatam.com): " DOMAIN_USER
 
 DB_NAME="wp$(date +%s)"
-DB_USER="$db_name"
-DB_PASSWORD=$(date | md5sum | cut -c '1-12')
+DB_USER="$DB_NAME"
+DB_PASSWORD=$(openssl rand -base64 12 | tr -d '+/' | head -c 1; openssl rand -base64 11)
 DOMAIN="$DOMAIN_USER"
 
 
 # Actualizar el sistema
-sudo apt update -y
-sudo apt upgrade -y
+#sudo apt update -y
+#sudo apt upgrade -y
 
 # Instalar Nginx
-sudo apt install nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
+#apt install nginx -y
+#systemctl enable nginx
+#systemctl start nginx
 
 # Instalar MySQL Server
-sudo apt install mysql-server -y
-sudo mysql_secure_installation <<EOF
+#apt install mysql-server -y
+mysql_secure_installation <<EOF
 
 y
 $DB_PASSWORD
@@ -42,25 +42,25 @@ y
 EOF
 
 # Instalar PHP 8.1 y extensiones
-sudo apt install php8.1-fpm php8.1 php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl php8.1-bcmath unzip -y
+#apt install php8.1-fpm php8.1 php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl php8.1-bcmath unzip memcached php8.1-memcache redis php8.1-redis -y
 
 # Configurar PHP
-sudo sed -i 's/upload_max_filesize =.*/upload_max_filesize = 1024M/' /etc/php/8.1/fpm/php.ini
-sudo sed -i 's/post_max_size =.*/post_max_size = 1200M/' /etc/php/8.1/fpm/php.ini
-sudo sed -i 's/memory_limit =.*/memory_limit = 512M/' /etc/php/8.1/fpm/php.ini
-sudo sed -i 's/max_execution_time =.*/max_execution_time = 600/' /etc/php/8.1/fpm/php.ini
-sudo sed -i 's/max_input_vars =.*/max_input_vars = 3000/' /etc/php/8.1/fpm/php.ini
-sudo sed -i 's/max_input_time =.*/max_input_time = 1000/' /etc/php/8.1/fpm/php.ini
+sed -i 's/upload_max_filesize =.*/upload_max_filesize = 1024M/' /etc/php/8.1/fpm/php.ini
+sed -i 's/post_max_size =.*/post_max_size = 1200M/' /etc/php/8.1/fpm/php.ini
+sed -i 's/memory_limit =.*/memory_limit = 512M/' /etc/php/8.1/fpm/php.ini
+sed -i 's/max_execution_time =.*/max_execution_time = 600/' /etc/php/8.1/fpm/php.ini
+sed -i 's/max_input_vars =.*/max_input_vars = 3000/' /etc/php/8.1/fpm/php.ini
+sed -i 's/max_input_time =.*/max_input_time = 1000/' /etc/php/8.1/fpm/php.ini
 
 # Reiniciar PHP-FPM
-sudo service php8.1-fpm restart
+service php8.1-fpm restart
 
 # Configurar Nginx
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo rm -rf /etc/nginx/sites-available/default
+rm -rf /etc/nginx/sites-enabled/default
+rm -rf /etc/nginx/sites-available/default
 
-# Crear configuración de Nginx para WordPress
-sudo tee /etc/nginx/sites-available/$DOMAIN > /dev/null <<EOF
+# Crear configuración de Nginx para WordPress y redirigirla al archivo
+cat <<EOF | sudo tee /etc/nginx/sites-available/$DOMAIN > /dev/null
 server {
     listen 80;
     listen [::]:80;
@@ -100,13 +100,13 @@ server {
 EOF
 
 # Habilitar el sitio en Nginx
-sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
 
 # Reiniciar Nginx
-sudo systemctl restart nginx
+systemctl restart nginx
 
 # Crear la base de datos y usuario de WordPress
-sudo mysql -u root -p <<MYSQL_SCRIPT
+mysql -u root -p <<MYSQL_SCRIPT
 CREATE DATABASE $DB_NAME;
 CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'localhost';
@@ -116,17 +116,18 @@ MYSQL_SCRIPT
 
 # Descargar e instalar WordPress
 cd /var/www/html
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar -zxvf latest.tar.gz --strip-components=1
-sudo rm -f latest.tar.gz
-sudo chown -R www-data:www-data /var/www/html/
-sudo chmod -R 755 /var/www/html/
+wget https://wordpress.org/latest.tar.gz
+tar -zxvf latest.tar.gz --strip-components=1
+rm -f latest.tar.gz
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
+rm /var/www/html/index.nginx-debian.html
 
 # Configurar wp-config.php
-sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-sudo sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
-sudo sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
-sudo sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/wp-config.php
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
+sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
+sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/wp-config.php
 
 # Mostrar las contraseñas generadas en el archivo de registro
 echo "Aquí tus datos:"
